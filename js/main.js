23a11081d2502860c7c70476d4790ed8c0921964
jonbullock <jonbullock@users.noreply.github.com>
2024-02-07 18:40:54 +0000

    deploy: 0fae72c9a8d76646ef109e0a129e5f7154210acd

--- js/main.js
+++ js/main.js
@@ -215,7 +215,7 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown').countdown('2024/02/03', function(event) {
+		$('#date-countdown').countdown('2024/02/10', function(event) {
 		  var $this = $(this).html(event.strftime(''
 		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
