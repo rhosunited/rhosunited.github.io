b06bdb929451b458eefd3eb6604396ed3f82ad9d
Jonathan Bullock <jonbullock@gmail.com>
2024-02-13 17:21:57 +0000

    Added news for Walking Sessions.

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,7 +215,7 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown').countdown('2024/02/12 05:00', function(event) {
+		$('#date-countdown').countdown('2024/02/17 14:00', function(event) {
 		  var $this = $(this).html(event.strftime(''
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
 		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
