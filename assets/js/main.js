9cb4b9a51cf3e6145f013f8517d72e60a7a27a62
Jonathan Bullock <jonbullock@gmail.com>
2024-02-18 01:09:53 +0000

    Moved next match info into data file. Added junior team social event details.

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,17 +213,4 @@ jQuery(document).ready(function($) {
 	};
 	siteStellar();
 
-	var siteCountDown = function() {
-
-		$('#date-countdown').countdown('2024/02/17 14:00', function(event) {
-		  var $this = $(this).html(event.strftime(''
-		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
-		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
-		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
-		});
-				
-	};
-	siteCountDown();
-
 });
